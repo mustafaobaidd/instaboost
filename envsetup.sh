3741b0c8fc72f37af404b744f35e1dd4c7fb824d
laithbilto <mrlaith44@gmail.com>
2023-12-16 20:56:51 +0300

    make envsetup do the python virtual enviroment as well

--- envsetup.sh
+++ envsetup.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+
+echo "setting up python"
+
+python -m venv .env
+source .env/bin/activate
+pip install instagrapi Pillow
+
+echo "Done!"
+
 echo "Exporting env variables"
-
-
 
 export INSTAUSERNAME="insert username here"
 echo $INSTAUSERNAME
