b48d536d461b8f7ee951595515c667d8b66ef5f9
laithbilto <mrlaith44@gmail.com>
2023-12-16 20:44:42 +0300

    add env setup and also have the creds as env variables

---
+++ envsetup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Exporting env variables"
+
+
+
+export INSTAUSERNAME="insert username here"
+echo $INSTAUSERNAME
+export INSTAPASSWORD="insert password here"
+echo $INSTAPASSWORD
+echo "Done"
